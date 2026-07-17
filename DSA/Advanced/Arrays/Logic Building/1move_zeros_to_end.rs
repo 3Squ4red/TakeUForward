2ec76d83c70d921dca65bd2e245ebdd4acc6cbec
3Squ4red <abhijeetranjan390@gmail.com>
2026-07-17 22:31:55 +0530

    add remove duplicates

--- DSA/Advanced/Arrays/Logic Building/1move_zeros_to_end.rs
+++ DSA/Advanced/Arrays/Logic Building/1move_zeros_to_end.rs
@@ -1,1 +1,20 @@
+fn main() {
+    let mut arr = [0, 0, 0, 0, 1, 1, 1, 1];
+    move_zeroes(&mut arr);
+    println!("{:?}", arr);
+}
 
+fn move_zeroes(arr: &mut [i32]) {
+    'outer: for i in 0..arr.len() {
+        if arr[i] == 0 {
+            let mut j = i;
+            while arr[j] == 0 {
+                j+=1;
+                if j == arr.len() {break 'outer}
+            }
+
+            arr.swap(i, j);
+
+        }
+    }
+}
--- DSA/Advanced/Arrays/Logic Building/2remove_duplicates_from_sorted_array.rs
+++ DSA/Advanced/Arrays/Logic Building/2remove_duplicates_from_sorted_array.rs
@@ -1,1 +1,23 @@
+fn main() {
+    let mut arr = [0, 0, 3, 3, 5, 6];
+    println!("{}", remove_duplicates(&mut arr));
+    println!("{:?}", arr);
+}
 
+fn remove_duplicates(arr: &mut [i32]) -> usize {
+    // The Sniper Guard
+    if arr.is_empty() {
+        return 0;
+    }
+
+    let mut i = 0;
+    
+    for j in 1..arr.len() {
+        if arr[j] != arr[i] {
+            i += 1; // Increment the slow pointer first
+            arr[i] = arr[j]; // Drop the new unique value into place
+        }
+    }
+    
+    i + 1
+}
